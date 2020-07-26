c5c2b11f08f10ed711b448f0e97469be2b064c26
Maciej Wojtasiak <maciekwojtasiak@gmail.com>
2020-07-26 14:53:08 +0200

    basic calculator funnctions

---
+++ app.js
@@ -0,0 +1,35 @@
+
+
+//calculator functions
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if (b === 0) alert("can't divide by 0!")
+    else return a / b;
+}
+
+function operate(operator, firstNumber, secondNumber) {
+    switch (operator) {
+        case '+':
+            return add(firstNumber, secondNumber);
+        case '-':
+            return subtract(firstNumber, secondNumber);
+        case '*':
+            return multiply(firstNumber, secondNumber);
+        case '/':
+            return divide(firstNumber, secondNumber);
+        default:
+            alert("Wrong operator");
+    }
+}
